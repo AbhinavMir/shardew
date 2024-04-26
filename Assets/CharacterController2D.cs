7e901c3a71ed9ada7b3ed77f03b4d878a6298750
Abhinav Srivastava <atg271@gmail.com>
2024-04-26 00:39:25 -0400

    wip

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -11,6 +11,8 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] float speed = 2f;
     Vector2 motionVector;
     Animator animator;
+    public Vector2 lastMotionVector;
+    public bool moving;
 
     void Awake()
     {
@@ -20,9 +22,35 @@ public class CharacterController2D : MonoBehaviour
 
     private void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Debug.Log("MOVING? " + moving);
         motionVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        animator.SetFloat("horizontal", Input.GetAxisRaw("Horizontal"));
-        animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
+
+        moving = horizontal != 0 || vertical != 0;
+        animator.SetBool("moving", moving);
+
+        Debug.Log(lastMotionVector);
+        if (horizontal != 0 || vertical != 0)
+        {
+            if (horizontal != 0)
+            {
+                Debug.Log("Horizontal");
+            }
+            else if (vertical != 0)
+            {
+                Debug.Log("Vertical");
+            }
+            lastMotionVector = new Vector2(horizontal, vertical).normalized;
+            animator.SetFloat("lastHorizontal", horizontal);
+            animator.SetFloat("lastVertical", vertical);
+        }
+        else
+        {
+        }
     }
 
     // Update is called once per frame
@@ -34,5 +62,18 @@ public class CharacterController2D : MonoBehaviour
     void Move()
     {
         rigidbody2d.velocity = motionVector * speed;
+        // Log detailed direction information
+        if (motionVector.x > 0)
+            Debug.Log("Moving Right");
+        else if (motionVector.x < 0)
+            Debug.Log("Moving Left");
+
+        if (motionVector.y > 0)
+            Debug.Log("Moving Up");
+        else if (motionVector.y < 0)
+            Debug.Log("Moving Down");
+
+        // Log speed
+        Debug.Log($"Speed: {speed}");
     }
 }
